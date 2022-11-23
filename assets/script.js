ae0ca76eaf7f7b3c5e6816c106dd6201bd93330c
William <william.deazevedopro@gmail.com>
2022-11-23 23:58:42 +0100

    fleche fonctionne lié aux images et paragraph

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,49 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+const leftArrow = document.querySelector(".arrow_left");
+const rightArrow = document.querySelector('.arrow_right');
+const dots = document.getElementsByClassName("dot")
+const slidNumber = slides.length;
+const banerImage = document.querySelector('.active_img');
+const banerParagraph = document.querySelector("#banner p");
+console.log(banerParagraph)
+var sourceImage = "./assets/images/slideshow/";
+let count = 0;
+
+function img() {
+	const image = slides[count].image;
+	// const image = `slide${count + 1}.jpg`
+	console.log(image)
+	banerImage.setAttribute("src", sourceImage + image )
+	const paragraph = slides[count].tagLine;
+	console.log(paragraph)
+	banerParagraph.innerHTML = paragraph;
+}
+
+
+function slideSuivant() {
+	dots[count].classList.remove("dot_selected");
+	if(count < slidNumber - 1)  {
+		count++
+	} else {
+		count = 0;
+	}
+	dots[count].classList.add("dot_selected");
+	img()
+}
+
+function slidePrecedent() {
+	dots[count].classList.remove("dot_selected");
+	if(count > 0)  {
+		count--
+	} else {
+		count = slidNumber - 1;
+	}
+	dots[count].classList.add("dot_selected");
+	img()
+}
+
+leftArrow.addEventListener("click", slidePrecedent);
+rightArrow.addEventListener("click", slideSuivant);
